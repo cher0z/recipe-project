6d83a9b54ca1f0a337983a8314ae4d6afab0ebd6
cher0z <reni.rasa@gmail.com>
2019-01-25 11:51:35 +0100

    initialized address of project

--- src/main/java/controllers/IndexController.java
+++ src/main/java/controllers/IndexController.java
@@ -1,4 +1,11 @@
 package controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        return "index";
+    }
 }
