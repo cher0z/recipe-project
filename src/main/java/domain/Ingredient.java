f1fb5ce21aeda648c6e9c07ec024c0671229137c
cher0z <reni.rasa@gmail.com>
2019-01-26 13:29:28 +0100

    created entity UnitOfMeasure established one-to-one relationship with Ingridient

--- src/main/java/domain/Ingredient.java
+++ src/main/java/domain/Ingredient.java
@@ -13,10 +13,11 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    //private UnitOfMeasure uom;
-
     @OneToMany
     private Recipe recipe;
+
+    @OneToOne (fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
 
     public Long getId() {
         return id;
@@ -48,5 +49,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
     }
 }
